ae09c95fb6320730120f1d446b98324d80ed06cb
Tulir Asokan <tulir@maunium.net>
2019-04-21 17:19:59 +0300

    Improve UI and code storage format

--- sfcb.py
+++ sfcb.py
@@ -13,8 +13,9 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from typing import Dict
+from typing import Dict, List, NamedTuple, Pattern
 from pkg_resources import resource_string
+import re
 import asyncio
 import json
 
@@ -29,14 +30,20 @@ from maubot.handlers import command, event
 
 STATE_SWITCH_FRIEND_CODES = EventType("xyz.maubot.switch.friendcodes", EventType.Class.STATE)
 
+Profile = NamedTuple("Profile", displayname=str, avatar_url=str)
+
 
 class SwitchFriendCodeBot(Plugin):
-    cache: Dict[RoomID, Dict[UserID, str]]
+    cache: Dict[RoomID, Dict[UserID, List[int]]]
+    profiles: Dict[RoomID, Dict[UserID, Profile]]
     cache_lock: Dict[RoomID, asyncio.Lock]
+    widget_tpl: Template
+    code_regex: Pattern = re.compile(r"^(?:SW-)?([0-9]{4})-?([0-9]{4})-?([0-9]{4})$")
 
     async def start(self) -> None:
         await super().start()
         self.cache = {}
+        self.profiles = {}
         self.cache_lock = {}
         self.webapp.add_get("/codes", self.get_widget)
         self.webapp.add_get("/codes.json", self.get_json)
@@ -54,7 +61,7 @@ class SwitchFriendCodeBot(Plugin):
             self.cache_lock[room_id] = lock
             return lock
 
-    async def get_codes(self, room_id: RoomID) -> Dict[UserID, str]:
+    async def get_codes(self, room_id: RoomID) -> Dict[UserID, List[int]]:
         async with self._lock(room_id):
             try:
                 return self.cache[room_id]
@@ -68,13 +75,36 @@ class SwitchFriendCodeBot(Plugin):
                 self.cache[room_id] = {}
             return self.cache[room_id]
 
+    async def get_profiles(self, room_id: RoomID) -> Dict[UserID, Profile]:
+        async with self._lock(room_id):
+            try:
+                return self.profiles[room_id]
+            except KeyError:
+                pass
+
+            try:
+                states = await self.client.get_state(room_id)
+            except MNotFound:
+                return {}
+            profiles = {}
+            for evt in states:
+                if evt.type == EventType.ROOM_MEMBER:
+                    profiles[UserID(evt.state_key)] = Profile(
+                        displayname=evt.content.displayname, avatar_url=evt.content.avatar_url)
+            self.profiles[room_id] = profiles
+            return profiles
+
     @command.new("code", help="Share your Switch friend code")
     @command.argument("code", required=True)
     async def set_code(self, evt: MessageEvent, code: str) -> None:
-        if code.startswith("SW-"):
-            code = code[len("SW-"):]
+        match = self.code_regex.fullmatch(code)
+        try:
+            data = [int(match.group(1)),int(match.group(2)), int(match.group(3))]
+        except (AttributeError, IndexError, ValueError):
+            await evt.reply("That does not look like a valid Switch friend code")
+            return
         cache = await self.get_codes(evt.room_id)
-        cache[evt.sender] = code
+        cache[evt.sender] = data
         await self.client.send_state_event(room_id=evt.room_id,
                                            event_type=STATE_SWITCH_FRIEND_CODES,
                                            content=cache)
@@ -82,7 +112,18 @@ class SwitchFriendCodeBot(Plugin):
 
     @event.on(STATE_SWITCH_FRIEND_CODES)
     async def handle_code(self, evt: StateEvent) -> None:
-        self.cache[evt.room_id] = evt.content.serialize()
+        async with self._lock(evt.room_id):
+            self.cache[evt.room_id] = evt.content.serialize()
+
+    @event.on(EventType.ROOM_MEMBER)
+    async def handle_membership(self, evt: StateEvent) -> None:
+        async with self._lock(evt.room_id):
+            self.profiles[evt.room_id][UserID(evt.state_key)] = Profile(
+                displayname=evt.content.displayname, avatar_url=evt.content.avatar_url)
+
+    def _get_download_url(self, mxc: str) -> str:
+        return (f"{self.client.api.base_url}/_matrix/media/r0/thumbnail/{mxc[6:]}"
+                f"?width=16&height=16&method=crop")
 
     async def get_widget(self, request: web.Request) -> web.Response:
         try:
@@ -91,7 +132,9 @@ class SwitchFriendCodeBot(Plugin):
             return web.Response(status=400, content_type="text/plain",
                                 text="Room ID query param missing")
         return web.Response(status=200, content_type="text/html",
-                            text=self.widget_tpl.render(codes=await self.get_codes(room_id)))
+                            text=self.widget_tpl.render(codes=await self.get_codes(room_id),
+                                                        profiles=await self.get_profiles(room_id),
+                                                        get_url=self._get_download_url))
 
     async def get_json(self, request: web.Request) -> web.Response:
         try:
